da8b0fe7c07b033000cbc39c60ec85cb29611614
Sreevaatsav Bavana <80395869+SreevaatsavB@users.noreply.github.com>
2022-11-25 13:03:29 +0530

    new client

--- client.py
+++ client.py
@@ -1,9 +1,11 @@
 import socket
 import os
 
-BUFFER_SIZE = 2048
-SEPARATOR = "-"
-HOST = "192.168.56.1"
+BUFFER_SIZE = 9216
+# HOST = "192.168.56.1"
+# HOST = "122.184.65.66"
+HOST = "10.59.232.84"
+
 PORT = 6666
 filename = "movies_info.csv"
 filesize = os.path.getsize(filename)
@@ -15,23 +17,25 @@ def client_prog():
     client_socket.connect((HOST, PORT))
     print("CONNECTED")
 
-    send_str = filename+SEPARATOR+str(filesize)
-    send_str = send_str.encode()
-    client_socket.send(send_str)
+    file = open(filename, "r")
+    data = file.read()
+    print(len(data))
+    
+    client_socket.send(filename.encode("utf-8"))
+    msg1 = client_socket.recv(BUFFER_SIZE).decode("utf-8")
+    print("SERVER SAYS :- ", msg1)
 
+    
+    client_socket.send(data.encode("utf-8"))
+    msg2 = client_socket.recv(BUFFER_SIZE).decode("utf-8")
+    print("SERVER SAYS :- ", msg2)
 
-    with open(filename, "rb") as f:
-        
-        while True:
+    file.close()
+
             
-            bytes_read = f.read(BUFFER_SIZE)
-            
-            if not bytes_read:
-                break
-            
-            client_socket.sendall(bytes_read)
-            
+    print("ALL THE CONTENTS OF THE FILES HAS BEEN SENT")
     client_socket.close()
+    print("DISCONNETED")
 
 if __name__ == "__main__":
     client_prog()
