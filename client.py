44eec5dc9a2344670994c3de410930504df4e8e8
Sreevaatsav Bavana <80395869+SreevaatsavB@users.noreply.github.com>
2022-11-25 15:00:43 +0530

    updated server-client

--- client.py
+++ client.py
@@ -4,36 +4,50 @@ import os
 BUFFER_SIZE = 9216
 # HOST = "192.168.56.1"
 # HOST = "122.184.65.66"
-HOST = "10.59.232.84"
 
+# The reciever's (IPv4) address (of wireless connection)
+HOST = "10.59.232.84" 
+HOST = '''Device's IP address'''
+
+# Port number
 PORT = 6666
-filename = "movies_info.csv"
-filesize = os.path.getsize(filename)
 
+# Filename of the file to be transferred
+filename = "movies_info.csv"
 
 def client_prog():
+
+    # Creating a socket for the client to send the file
     client_socket = socket.socket()
+
+    # Connecting to the server with TCP protocol
     print("CONNECTING TO", HOST, ":", PORT)
     client_socket.connect((HOST, PORT))
     print("CONNECTED")
 
+    # reading the file 
     file = open(filename, "r")
     data = file.read()
-    print(len(data))
-    
+
+    # We use the utf-8 encoding and decoding to transfer data via TCPs
+
+    # Sending file name to the server(reciever), so that it can save the data into that file    
     client_socket.send(filename.encode("utf-8"))
+    # Reading the message Server sends
     msg1 = client_socket.recv(BUFFER_SIZE).decode("utf-8")
     print("SERVER SAYS :- ", msg1)
 
-    
+    # Sending the data 
     client_socket.send(data.encode("utf-8"))
+    # Reading the message Server sends
     msg2 = client_socket.recv(BUFFER_SIZE).decode("utf-8")
     print("SERVER SAYS :- ", msg2)
 
-    file.close()
-
-            
+    # Closing file after all the files contents have been sent
     print("ALL THE CONTENTS OF THE FILES HAS BEEN SENT")
+    file.close()
+        
+    # Closing the client socket
     client_socket.close()
     print("DISCONNETED")
 
--- server.py
+++ server.py
@@ -5,19 +5,24 @@ import os
 # SERVER_HOST = "192.168.56.1"
 # SERVER_HOST = "122.184.65.66"
 SERVER_HOST = "10.59.232.84"
+SERVER_HOST = '''Device's IP address'''
 # SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 6666
-
 BUFFER_SIZE = 9216
-SEPARATOR = "-"
+
 
 def server_prog():
+    # Creating a server socket to recieve the data
     server_socket = socket.socket()
+
+    # making a server socket
     server_socket.bind((SERVER_HOST, SERVER_PORT))
 
+    # 6 unaccepted connections that the system will allow before refusing new connections.
     server_socket.listen(6)
     print("LISTENING AS ",SERVER_HOST,":",SERVER_PORT)
 
+    # Accepting the connection made
     client_socket, ADDRESS = server_socket.accept()
 
     print(ADDRESS, "IS CONNECTED")
@@ -25,14 +30,17 @@ def server_prog():
     filename = client_socket.recv(BUFFER_SIZE).decode("utf-8")
     print("FILENAME IS ", filename)
     file = open(filename, "w")
+    # After the filename was succesfully recieved, server opens a new file with same filename and sends a message
     client_socket.send("FILE NAME RECIEVED".encode("utf-8"))
 
-
+    # Recieving the data from sender(client socket)
     data = client_socket.recv(BUFFER_SIZE).decode("utf-8")
     print("Length of the data recieved :- ",len(data))
+    # Writing that daat into the file opened
     file.write(data)
     client_socket.send("DATA RECIEVED".encode("utf-8"))
     
+    # Closing the sockets after sucessfull data transfer
     print("File recieved and read")        
     client_socket.close()
     print("FILE TRANSFER DONE")
